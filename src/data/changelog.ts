3f97d85951e571e2adbee255ac8968dcb214c322
Eric-Geek <ericwalker0316@gmail.com>
2025-08-24 03:25:22 +0800

    在changelog中添加了版本1.5.0的更新记录，新增了Image-converter功能。

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -7,6 +7,14 @@ export interface ChangelogEntry {
 
 export const changelog: ChangelogEntry[] = [
   {
+    version: '1.5.0',
+    date: '2025-08-24',
+    type: 'feature',
+    changes: [
+      'Added Image-converter',
+    ]
+  },
+  {
     version: '1.4.0',
     date: '2025-08-01',
     type: 'feature',
